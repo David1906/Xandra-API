1841a6c1849e189e0ef4d58512e155cda7c1ba74
David Ascencio <david1906@outlook.com>
2023-02-16 13:01:12 -0600

    Fix: Absolute path to .env

--- xandra_api.py
+++ xandra_api.py
@@ -1,4 +1,4 @@
-import logging
+import logging, os
 from flask import Flask
 from flask_restful import Api
 from waitress import serve
@@ -7,7 +7,7 @@ from Utils.FileWatchdog import FileWatchdog
 from Utils.SfcEventHandler import SfcEventHandler
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
+config = dotenv_values(os.path.dirname(os.path.abspath(__file__)) + "/.env")
 logging.basicConfig(filename="log.txt", level=logging.ERROR)
 
 app = Flask(__name__)
@@ -16,5 +16,5 @@ api.add_resource(YieldCalculator, "/yield")
 
 if __name__ == "__main__":
     FileWatchdog(SfcEventHandler()).start(config["FBT_LOGS_PATH"])
-    #app.run(debug=True)
+    # app.run(debug=True)
     serve(app, host="0.0.0.0", port=5000)
